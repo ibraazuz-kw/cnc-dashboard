2813c6161ab6458e87bf5900f2d22feed4c01088
ibraazuz-kw <ibraazuz@gmail.com>
2026-01-20 23:01:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,31 +6,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
 
-// الرئيسية
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "login.html")));
+app.get("/client", (req, res) => res.sendFile(path.join(__dirname, "client.html")));
+app.get("/admin", (req, res) => res.sendFile(path.join(__dirname, "admin.html")));
 
-// صفحة الدخول
-app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "login.html"));
-});
-
-// صفحة العميل
-app.get("/client", (req, res) => {
-  res.sendFile(path.join(__dirname, "client.html"));
-});
-
-// صفحة الأدمن
-app.get("/admin", (req, res) => {
-  res.sendFile(path.join(__dirname, "admin.html"));
-});
-
-// أي رابط ثاني = Not Found (لا يرجع للبوابة)
-app.get("*", (req, res) => {
-  res.status(404).send("Not Found");
-});
-
-app.listen(PORT, () => {
-  console.log("Server running on port:", PORT);
-});
+app.listen(PORT, () => console.log("Server running on port:", PORT));
