09733345810ed1e80f309db3fc3e1353772129a2
ibraazuz-kw <ibraazuz@gmail.com>
2026-01-21 00:06:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,20 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
-app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "login.html")));
-app.get("/client", (req, res) => res.sendFile(path.join(__dirname, "client.html")));
-app.get("/admin", (req, res) => res.sendFile(path.join(__dirname, "admin.html")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-app.listen(PORT, () => console.log("Server running on port:", PORT));
+app.get("/client", (req, res) => {
+  res.sendFile(path.join(__dirname, "client.html"));
+});
+
+app.get("/admin", (req, res) => {
+  res.sendFile(path.join(__dirname, "admin.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, () => console.log("Running on port:", PORT));
