e7c9a4685e2261ccbc092a1c690b7a36e5b60e48
ibraazuz-kw <ibraazuz@gmail.com>
2026-01-21 02:07:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// يخدم كل الملفات (html/css/js)
 app.use(express.static(__dirname));
 
+// الصفحة الرئيسية
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
+// صفحات ثابتة
 app.get("/client", (req, res) => {
   res.sendFile(path.join(__dirname, "client.html"));
 });
@@ -18,8 +21,11 @@ app.get("/admin", (req, res) => {
   res.sendFile(path.join(__dirname, "admin.html"));
 });
 
+// fallback
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(PORT, () => console.log("Running on port:", PORT));
+app.listen(PORT, () => {
+  console.log("Server running on port:", PORT);
+});
