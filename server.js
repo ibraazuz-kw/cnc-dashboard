b0dca3950561af2ed0d3078870f65d1b885d3bb8
ibraazuz-kw <ibraazuz@gmail.com>
2026-01-23 11:28:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const path = require("path");
-const app = express();
 
-app.use(express.json({ limit: "20mb" }));
-app.use(express.urlencoded({ extended: true, limit: "20mb" }));
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
 
-// صفحات مباشرة
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
-app.get("/client.html", (req, res) => res.sendFile(path.join(__dirname, "client.html")));
-app.get("/admin.html", (req, res) => res.sendFile(path.join(__dirname, "admin.html")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Running on port", PORT));
+app.listen(PORT, () => {
+  console.log("Server running on port:", PORT);
+});
