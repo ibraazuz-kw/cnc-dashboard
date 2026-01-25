d9dc20544f5bdd17d8a7f01d559be58826b718c2
ibraazuz-kw <ibraazuz@gmail.com>
2026-01-26 01:23:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,23 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname));
+// Serve static files (html/css/js)
+app.use(express.static(path.join(__dirname)));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log("Server running on port:", PORT);
+app.get("/client.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "client.html"));
 });
+
+app.get("/admin.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "admin.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on port:", PORT));
