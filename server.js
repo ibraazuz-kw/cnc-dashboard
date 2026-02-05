8e563924110dba1582440a9d2bd88eac7abd6b5d
ibraazuz-kw <ibraazuz@gmail.com>
2026-02-05 18:19:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,20 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req,res)=>{
-  res.sendFile(path.join(__dirname,"public","index.html"));
+let ORDERS = [];
+
+app.post("/api/order", (req,res)=>{
+  ORDERS.push(req.body);
+  res.json({ok:true});
 });
 
-app.listen(PORT,()=>{
-  console.log("Server running on http://localhost:"+PORT);
+app.get("/api/orders", (req,res)=>{
+  res.json(ORDERS);
 });
+
+app.listen(PORT, ()=>console.log("Running on",PORT));
