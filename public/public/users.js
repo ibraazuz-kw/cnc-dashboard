78cded55f3c918a6fa2a2fb26000761be0a4b45f
ibraazuz-kw <ibraazuz@gmail.com>
2026-05-13 19:45:22 +0300

    Create public/users.js

---
+++ public/public/users.js
@@ -0,0 +1,48 @@
+const users = [
+
+/* =========================
+   Admin
+========================= */
+
+{
+username:"admin",
+password:"1234",
+role:"admin",
+name:"PRO DESIGN ADMIN"
+},
+
+/* =========================
+   Designer
+========================= */
+
+{
+username:"designer",
+password:"2222",
+role:"designer",
+name:"PRO DESIGN DESIGNER"
+},
+
+/* =========================
+   Factory
+========================= */
+
+{
+username:"factory",
+password:"3333",
+role:"factory",
+name:"PRO DESIGN FACTORY"
+},
+
+/* =========================
+   Clients
+========================= */
+
+{
+username:"client1",
+password:"1111",
+role:"client",
+clientId:"client_101",
+name:"PRO DESIGN CLIENT"
+}
+
+];
