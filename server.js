18a8d1ace35add70ec34b089f49cb0c52851155f
ibraazuz-kw <ibraazuz@gmail.com>
2026-05-15 00:51:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,41 @@
 const express = require("express");
+const path = require("path");
 
-express().listen(3000);
+const app = express();
+
+/* =========================
+   Public Folder
+========================= */
+
+app.use(
+express.static(
+path.join(__dirname, "public")
+)
+);
+
+/* =========================
+   Main Route
+========================= */
+
+app.get("/", (req, res) => {
+
+res.sendFile(
+path.join(__dirname, "public", "login.html")
+);
+
+});
+
+/* =========================
+   Server Start
+========================= */
+
+const PORT =
+process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+
+console.log(
+"🚀 PRO DESIGN ERP RUNNING"
+);
+
+});
