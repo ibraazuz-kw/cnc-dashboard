5d530df4d9115bc00ba0a4b85065ac06a691a71e
ibraazuz-kw <ibraazuz@gmail.com>
2026-05-15 01:49:42 +0300

    Update data.js

--- public/data.js
+++ public/data.js
@@ -1,27 +1,52 @@
-let orders = JSON.parse(localStorage.getItem("orders")) || [
+let orders = JSON.parse(
+localStorage.getItem("orders")
+) || [
 
-  {
-    id: 1,
-    company: "شركة باب السيف",
-    design: "باب خارجي مودرن",
-    sheets: "3 شيت ألمنيوم",
-    size: "300 × 100",
-    price: "450",
-    status: "بانتظار الاعتماد"
-  },
+{
+id:1,
 
-  {
-    id: 2,
-    company: "شركة الهاجري",
-    design: "بوابة CNC",
-    sheets: "5 شيتات",
-    size: "400 × 220",
-    price: "780",
-    status: "قيد التصنيع"
-  }
+company:"PRO DESIGN",
+
+design:"باب خارجي مودرن",
+
+sheets:"3 شيت ألمنيوم",
+
+size:"300 × 100",
+
+price:"450",
+
+status:"بانتظار الاعتماد"
+
+},
+
+{
+id:2,
+
+company:"PRO DESIGN",
+
+design:"بوابة CNC",
+
+sheets:"5 شيتات",
+
+size:"400 × 220",
+
+price:"780",
+
+status:"قيد التصنيع"
+
+}
 
 ];
 
-function saveOrders() {
-  localStorage.setItem("orders", JSON.stringify(orders));
+/* =========================
+   Save Orders
+========================= */
+
+function saveOrders(){
+
+localStorage.setItem(
+"orders",
+JSON.stringify(orders)
+);
+
 }
