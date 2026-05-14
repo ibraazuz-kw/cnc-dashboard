a7cfe9691206d4cbb5745b02ceb3cb5a27e7df8c
ibraazuz-kw <ibraazuz@gmail.com>
2026-05-14 23:56:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,3 @@
 const express = require("express");
-const path = require("path");
 
-const app = express();
-
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "login.html"));
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log("PRO DESIGN RUNNING");
-});
+express().listen(3000);
